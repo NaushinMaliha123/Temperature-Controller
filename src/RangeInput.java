3ff2bbebd6b98c301d279986e3950a4086101154
NaushinMaliha123 <tiashamaliha@gmail.com>
2024-05-05 11:49:04 +0600

    commit

---
+++ src/RangeInput.java
@@ -0,0 +1,34 @@
+public class RangeInput {
+    private int currentTemperature, minTemperature ,maxTemperature;
+
+    private void TemperatureControl() {
+        currentTemperature = (minTemperature + maxTemperature) / 2;
+    }
+
+
+    public  RangeInput(){
+        maxTemperature = 60;
+        minTemperature = 80;
+        TemperatureControl();
+    }
+
+    public RangeInput(int max,int min){
+        {
+            maxTemperature = max;
+            minTemperature = min;
+            TemperatureControl();
+        }
+    }
+
+    public void Up(){
+         currentTemperature = Math.min(currentTemperature + 1,maxTemperature);
+    }
+    public void Down(){
+        currentTemperature = Math.max(currentTemperature - 1,minTemperature);
+    }
+    public int getCurrentTemperature(){
+        return currentTemperature;
+    }
+
+
+}
---
+++ src/RangeInputTest.java
@@ -0,0 +1,62 @@
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RangeInputTest {
+    RangeInput passengerTemperature1, passengerTemperature2, driverTemperature1;
+
+    @BeforeEach
+
+    void setUp() {
+        passengerTemperature1 = new RangeInput(70, 50);
+        passengerTemperature1 = new RangeInput(80,60);
+        driverTemperature1= new RangeInput(90,70);
+
+    }
+
+    @Test
+    void getCurrentTemperatureForPassenger1() {
+        passengerTemperature1.Up();
+        passengerTemperature1.Up();
+        passengerTemperature1.Down();
+        passengerTemperature1.Up();
+        assertEquals(72,passengerTemperature1.getCurrentTemperature() );
+
+    }
+
+    @Test
+    void getTemperatureForPassenger2() {
+        passengerTemperature2.Up();
+        passengerTemperature2.Up();
+        passengerTemperature2.Up();
+        passengerTemperature2.Down();
+        passengerTemperature2.Down();
+        assertEquals(71, passengerTemperature2.getCurrentTemperature() );
+    }
+
+
+    @Test
+    void getTemperatureForDriver1() {
+        driverTemperature1.Up();
+        driverTemperature1.Up();
+        driverTemperature1.Up();
+        driverTemperature1.Down();
+        driverTemperature1.Down();
+        assertEquals(80, driverTemperature1.getCurrentTemperature());
+    }
+
+
+
+    @Test
+    void up() {
+    }
+
+    @Test
+    void down() {
+    }
+
+    @Test
+    void getCurrentTemperature() {
+    }
+}
